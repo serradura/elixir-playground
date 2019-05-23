a12a061b54c191ece710b033f83b23e88a65bfe3
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-05-23 00:22:01 -0300

    Improve count-labs-LOC.sh

--- count-labs-LOC.sh
+++ count-labs-LOC.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 
-find -E labs -iregex ".*\.(exs|ex)" -print \
-  | grep -v fibonacci/deps \
-  | sort \
-  | xargs wc -l
+exclude_dir_and_count_elixir_file_lines() {
+  EXCLUDE_DIR=$1
+  FOLDER=$2
+
+  find -E ${FOLDER} -iregex ".*\.(exs|ex)" -print \
+    | grep -v ${EXCLUDE_DIR} \
+    | sort \
+    | xargs wc -l
+}
+
+count_elixir_files_lines_in() {
+  FOLDER=$1
+
+  find -E ${FOLDER} -iregex ".*\.(exs|ex)" \
+    | sort \
+    | xargs wc -l
+}
+
+count_elixir_labs_lines_per_file() {
+  exclude_dir_and_count_elixir_file_lines 'fibonacci/deps' 'labs'
+}
+
+count_elixir_alchemistcamp_lines_per_file() {
+  count_elixir_files_lines_in 'alchemist-camp'
+}
+
+echo ''
+echo 'labs'
+echo '----'
+echo ''
+
+count_elixir_labs_lines_per_file
+
+echo ''
+echo 'alchemist-camp'
+echo '--------------'
+echo ''
+
+count_elixir_alchemistcamp_lines_per_file
+
+echo ''
+
+LABS_TOTAL=$(count_elixir_labs_lines_per_file | tail -1 | awk '{print $1}')
+ALCHEMISTCAMP_TOTAL=$(count_elixir_alchemistcamp_lines_per_file | tail -1 | awk '{print $1}')
+
+echo "Total of lines: $((LABS_TOTAL + ALCHEMISTCAMP_TOTAL))"
