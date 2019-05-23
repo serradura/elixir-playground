4211e4219d7ef0ad5c5e471e3426778f68320491
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-05-23 00:45:24 -0300

    Count the LOC of little-alchemist elixir files

--- count-labs-LOC.sh
+++ count-labs-LOC.sh
@@ -22,16 +22,20 @@ count_elixir_labs_lines_per_file() {
   exclude_dir_and_count_elixir_file_lines 'fibonacci/deps' 'labs'
 }
 
+count_elixir_littlealchemist_lines_per_file() {
+  count_elixir_files_lines_in 'little-alchemist'
+}
+
 count_elixir_alchemistcamp_lines_per_file() {
   count_elixir_files_lines_in 'alchemist-camp'
 }
 
 echo ''
-echo 'labs'
-echo '----'
+echo 'little-alchemist'
+echo '----------------'
 echo ''
 
-count_elixir_labs_lines_per_file
+count_elixir_littlealchemist_lines_per_file
 
 echo ''
 echo 'alchemist-camp'
@@ -41,8 +45,16 @@ echo ''
 count_elixir_alchemistcamp_lines_per_file
 
 echo ''
+echo 'labs'
+echo '----'
+echo ''
 
+count_elixir_labs_lines_per_file
+
+echo ''
+
+LITTLE_ALCHEMIST_TOTAL=$(count_elixir_littlealchemist_lines_per_file | tail -1 | awk '{print $1}')
+ALCHEMIST_CAMP_TOTAL=$(count_elixir_alchemistcamp_lines_per_file | tail -1 | awk '{print $1}')
 LABS_TOTAL=$(count_elixir_labs_lines_per_file | tail -1 | awk '{print $1}')
-ALCHEMISTCAMP_TOTAL=$(count_elixir_alchemistcamp_lines_per_file | tail -1 | awk '{print $1}')
 
-echo "Total of lines: $((LABS_TOTAL + ALCHEMISTCAMP_TOTAL))"
+echo "Total of lines: $((LABS_TOTAL + ALCHEMIST_CAMP_TOTAL + LITTLE_ALCHEMIST_TOTAL))"
