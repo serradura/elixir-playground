7e6ac7ba02da9ae027fd0ecd1787f056e5321696
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-05-23 23:20:09 -0300

    Change the status code to 404 and render the Error View

--- phoenix/hello/lib/hello_web/controllers/page_controller.ex
+++ phoenix/hello/lib/hello_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    # render(conn, :index)
+
+    conn
+    |> put_status(:not_found)
+    |> put_view(HelloWeb.ErrorView)
+    |> render("404.html")
   end
 end
